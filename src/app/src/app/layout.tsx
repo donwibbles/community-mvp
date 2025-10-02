4bace5eaf965c0f1c4c6b369db51b9c8a8c4c273
donwibbles <vm.rubiorivera@gmail.com>
2025-10-02 11:51:20 -0700

    Delete src/app directory

--- src/app/src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Community MVP" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{fontFamily: "sans-serif"}}>{children}</body>
-    </html>
-  );
-}
