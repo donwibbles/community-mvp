68a2cf6256500d29ee217362e98cc1509156d0df
donwibbles <vm.rubiorivera@gmail.com>
2025-10-03 17:43:35 -0700

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,12 +4,39 @@ import ChannelList from "./ChannelList";
 
 export default function Sidebar() {
   return (
-    <aside style={{ borderRight: "1px solid #eee", padding: 12, display: "flex", flexDirection: "column", gap: 8 }}>
+    <aside
+      style={{
+        borderRight: "1px solid #eee",
+        padding: 12,
+        display: "flex",
+        flexDirection: "column",
+        gap: 8,
+        minWidth: 200,
+      }}
+    >
       <div style={{ fontWeight: 600 }}>Community</div>
+
+      {/* Main navigation */}
       <nav style={{ display: "grid", gap: 6 }}>
-        <Link href="/app" className="hover:underline">Home</Link>
+        <Link href="/app" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/app/events" className="hover:underline">
+          Events
+        </Link>
       </nav>
-      <div style={{ marginTop: 8, fontSize: 12, color: "#666", textTransform: "uppercase" }}>Channels</div>
+
+      {/* Channels section */}
+      <div
+        style={{
+          marginTop: 12,
+          fontSize: 12,
+          color: "#666",
+          textTransform: "uppercase",
+        }}
+      >
+        Channels
+      </div>
       <ChannelList />
     </aside>
   );
